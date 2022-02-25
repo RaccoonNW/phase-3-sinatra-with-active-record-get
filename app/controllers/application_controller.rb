5cfed6fb68d50bc8eadb1be272af75d84b0aad49
Gabriel <shalitmontagne.g@gmail.com>
2022-02-25 14:09:01 -0700

    Completed Assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+    # get all the games from the database
+    # return a JSON response with an array of all the game data
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
